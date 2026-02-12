f47204d71bed95d42afc11c2a26c5893b0644336
saiftel83 <saiftel83@gmail.com>
2026-02-12 06:09:11 +0300

    main new

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://isdywilbkneoxhotovts.supabase.co"
-const supabaseAnonKey = "sb_publishable_mryW3oqzYneGH1p54Pwf7A_eU1RV1W2"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzZHl3aWxia25lb3hob3RvdnRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NDM2ODgsImV4cCI6MjA4NjMxOTY4OH0.xqS35yKh6Qbp2nXiqM23hxqaFV7sAKZCT9jAiHtLMB4"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
